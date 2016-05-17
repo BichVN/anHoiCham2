802c62aeb9d363fe20bfad133674c6175b338b7f
BichVN <tieuke107@gmail.com>
2016-05-17 22:08:38 +0700

    addInterface#1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,13 @@ class UsersController < ApplicationController
 	before_action :user_signed_in?, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
   def show
-      @user = User.find(params[:id])
-      @menus = @user.menus.paginate(page: params[:page])
-      @comment = Comment.new
-      @tag = Tag.new
+    @user = User.find(params[:id])
+    @menus = @user.menus.paginate(page: params[:page])
+    @comment = Comment.new
+    @tag = Tag.new
+  end
+
+  def edit
   end
 
   def following

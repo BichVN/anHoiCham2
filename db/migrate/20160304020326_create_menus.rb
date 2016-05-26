b2178ac938f0d384ef65736e3b7c1cb8aa4e1ada
vietnezo <viet@verve-inc.jp>
2016-05-26 23:18:33 +0900

    update database

--- db/migrate/20160304020326_create_menus.rb
+++ db/migrate/20160304020326_create_menus.rb
@@ -6,6 +6,7 @@ class CreateMenus < ActiveRecord::Migration
       t.references :user, index: true, foreign_key: true
       t.references :tag, index: true, foreign_key: true
       t.integer :status, default: 0
+      t.integer :ask_user_id,
       t.timestamps null: false
     end
     add_index :menus, [:user_id, :created_at]
